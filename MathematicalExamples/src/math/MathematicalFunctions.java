32f2a18cc7a0ca629cafd7acd9a6953a573cebfe
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-10-27 02:53:46 +0200

    Mathematical Functions in Java

---
+++ MathematicalExamples/src/math/MathematicalFunctions.java
@@ -0,0 +1,199 @@
+package math;
+
+public class MathematicalFunctions {
+	
+	double sineFunc(double radians) {
+		double result = Math.sin(radians);
+		return result;
+	}
+	
+	double cosineFunc(double radians) {
+		double result = Math.cos(radians);
+		return result;
+	}
+	
+	double tangentFunc(double radians) {
+		double result = Math.tan(radians);
+		return result;
+	}
+	
+	double arcsineFunc(double radians) {
+		double result = Math.asin(radians);
+		return result;
+	}
+	
+	double arccosineFunc(double radians) {
+		double result = Math.acos(radians);
+		return result;
+	}
+	
+	double arctangentFunc(double radians) {
+		double result = Math.atan(radians);
+		return result;
+	}
+	
+	double arctangent2Func(double arg1radians, double arg2radians) {
+		double result = Math.atan2(arg1radians, arg2radians);
+		return result;
+	}
+	
+	double toRadiansFunc(double argument) {
+		double result = Math.toRadians(argument);
+		return result;
+	}
+	
+	double toDegriesFunc(double argument) {
+		double result = Math.toDegrees(argument);
+		return result;
+	}
+	
+	double toIntExactFunc(long argument) {
+		double result = Math.toIntExact(argument);
+		return result;
+	}
+	
+	double showPi() {
+		double mypi = Math.PI;
+		return mypi;
+	}
+	
+	double shoeE() {
+		double mye = Math.E;
+		return mye;
+	}
+	
+	int absoluteIntegerFunc(int argument) {
+		int result = Math.abs(argument);
+		return result;
+	}
+	
+	long absoluteLongFunc(long argument) {
+		long result = Math.abs(argument);
+		return result;
+	}
+	
+	float absoluteFloatFunc(float argument) {
+		float result = Math.abs(argument);
+		return result;
+	}
+	
+	double absoluteDoubleFunc(double argument) {
+		double result = Math.abs(argument);
+		return result;
+	}
+	
+	int maximumIntegerFunc(int argument1, int argument2) {
+		int result = Math.max(argument1, argument2);
+		return result;
+	}
+	
+	long maximumLongFunc(long argument1, long argument2) {
+		long result = Math.max(argument1, argument2);
+		return result;
+	}
+	
+	float maximumFloatFunc(float argument1, float argument2) {
+		float result = Math.max(argument1, argument2);
+		return result;
+	}
+	
+	double maximumDoubleFunc(double argument1, double argument2) {
+		double result = Math.max(argument1, argument2);
+		return result;
+	}
+	
+	int minimumIntegerFunc(int argument1, int argument2) {
+		int result = Math.min(argument1, argument2);
+		return result;
+	}
+	
+	long minimumLongFunc(long argument1, long argument2) {
+		long result = Math.min(argument1, argument2);
+		return result;
+	}
+	
+	float minimumFloatFunc(float argument1, float argument2) {
+		float result = Math.min(argument1, argument2);
+		return result;
+	}
+	
+	double minimumDoubleFunc(double argument1, double argument2) {
+		double result = Math.min(argument1, argument2);
+		return result;
+	}
+	
+	double ceilFunc(double argument) {
+		double result = Math.ceil(argument);
+		return result;
+	}
+	
+	double floorFunc(double argument) {
+		double result = Math.floor(argument);
+		return result;
+	}
+	
+	float roundFloatFunc(float argument) {
+		float result = Math.round(argument);
+		return result;
+	}
+	
+	double roundDoubleFunc(double argument) {
+		double result = Math.round(argument);
+		return result;
+	}
+	
+	double rintFunc(double argument) {
+		double result = Math.rint(argument);
+		return result;
+	}
+	
+	double squareRootFunc(double argument) {
+		double result = Math.sqrt(argument);
+		return result;
+	}
+	
+	double cubeRootFunc(double argument) {
+		double result = Math.cbrt(argument);
+		return result;
+	}
+	
+	double powerFunc(double base, double exponent) {
+		double result = Math.pow(base, exponent);
+		return result;
+	}
+	
+	double squareRootOfTwoArguments(double argument1, double argument2) {
+		double result = Math.hypot(argument1, argument2);
+		return result;
+	}
+	
+	double eraisedToPowerOfArgument(double argument) {
+		double result = Math.exp(argument);
+		return result;
+	}
+	
+	double eraisedToPowerOfArgumentMinusOne(double argument) {
+		double result = Math.expm1(argument);
+		return result;
+	}
+	
+	double naturalLogarithmBaseE(double argument) {
+		double result = Math.log(argument);
+		return result;
+	}
+	
+	double naturalLoratithmBaseEOfArgumentPlusOne(double argument) {
+		double result = Math.log1p(argument);
+		return result;
+	}
+	
+	double logarithmBase10(double argument) {
+		double result = Math.log10(argument);
+		return result;
+	}
+	
+	double randomFunc() {
+		double result = Math.random();
+		return result;
+	}
+}
